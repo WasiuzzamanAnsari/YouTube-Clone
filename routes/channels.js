49dda53dc79c0d119033d9c7cb6a76c207bbadaf
WasiuzzamanAnsari <ansariwasi117@gmail.com>
2025-05-07 02:07:18 +0530

    createNewChannel and getChannel api's created

--- routes/channels.js
+++ routes/channels.js
@@ -1,6 +1,11 @@
 import express from "express";
+import { verifyToken } from "../middlewares/verifyToken.js";
+import { createNewChannel, getChannel } from "../controllers/channel.js";
 
 const router = express.Router();
 
+router.post("/", verifyToken, createNewChannel);
+
+router.get("/:userId", getChannel);
 
 export default router;
