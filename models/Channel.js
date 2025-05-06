6f943d79a2c76e12ed5777cd8b652443f60e21b1
WasiuzzamanAnsari <ansariwasi117@gmail.com>
2025-05-06 19:53:47 +0530

    Created channel schema

---
+++ models/Channel.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const ChannelSchema = new mongoose.Schema(
+    {
+        userId:{
+            type:String,
+            required: true,
+            unique: true
+        },
+
+        name: {
+            type: String,
+            required: true
+        },
+
+        description: {
+            type:String
+        },
+
+        subscribers: {
+            type: Number,
+            default: 0
+        },
+
+        videos: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Video"
+            },
+        ],
+    },
+    {
+        timestamps:true
+    }
+);
+
+export default mongoose.model("Channel",ChannelSchema);
